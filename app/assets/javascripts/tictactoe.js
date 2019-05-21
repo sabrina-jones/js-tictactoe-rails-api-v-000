a51f1ba65dad89c84ae4e2ecb29dcaff08e2f120
Sabrina Jones <sabrina.jones87@gmail.com>
2019-05-21 21:51:36 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,35 @@
-// Code your JavaScript / jQuery solution here
+$(document).ready(function() {
+   attachListeners()
+});
+
+
+let turn = 0
+
+function player(){
+  //Returns 'X' when the turn variable is even and 'O' when it is odd
+   if (turn%2 === 0){
+     return "X"
+   }
+   else{
+     return "O"
+   }
+}
+
+function updateState(square){
+//Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
+ let player_turn = player()
+ $(square).text(player_turn)
+}
+
+function attachListeners(){
+
+  $(document).on('keydown', function(key) {
+    //console.log(key.which)
+    //debugger
+  if(key.which == 83){
+      alert('s was pressed');
+  }
+});
+
+
+}
