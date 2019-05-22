47e8d16c5a224f306ec64ab983ecc71ba1df95c7
Sabrina Jones <sabrina.jones87@gmail.com>
2019-05-22 01:44:10 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
    attachListeners()
 });
 
+let WINNING_COMBOS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
+let currentGame = 0
 let turn = 0
 
 function player(){
@@ -17,19 +19,80 @@ function player(){
 
 function updateState(square){
 //Invokes player() and adds the returned string ('X' or 'O') to the clicked square on the game board.
- let player_turn = player()
- $(square).text(player_turn)
+  let player_turn = player()
+  $(square).text(player_turn)
+}
+
+function setMessage(string) {
+    $('#message').text(string)
+}
+
+function checkWinner() {
+  let board = []
+  let winner = false
+
+  $('td').text(function(index, td){
+  	board[index] = td
+  })
+
+  WINNING_COMBOS.forEach(function(combo) {
+    if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+      setMessage(`Player ${board[combo[0]]} Won!`)
+      winner = true
+    }
+  })
+  return winner
+}
+
+function doTurn(square) {
+
+  updateState(square)
+  turn++
+
+  if (checkWinner()) {
+    saveGame()
+    resetBoard()
+  } else if (turn === 9){
+    setMessage("Tie game.")
+    saveGame()
+    resetBoard()
+  }
+}
+
+function resetBoard(){
+  turn = 0;
+  $('td').empty();
+  currentGame = 0;
 }
 
 function attachListeners(){
 
-  $(document).on('keydown', function(key) {
-    //console.log(key.which)
+  //$('td').on('click', function() {
+
     //debugger
-  if(key.which == 83){
-      alert('s was pressed');
-  }
-});
+  //if(key.which == 83){
+    //  alert('s was pressed');
+  //}
+//});
+  $('td').click(function(){
+    console.log(this)
+    if (!$.text(this) && !checkWinner()) {
+        doTurn(this)
+      }
+    })
+
+  $('#previous').click(function(){
+    previousGames()
+  });
+
+  $('#save').click(function(){
+    saveGame()
+  });
+
+  $('#clear').click(function(){
+    resetBoard()
+  });
+
 
 
 }
